01287a2d94e7d2e08198f2d3dde9481a2b0516e9
fuvkk <93823376+fuvkk@users.noreply.github.com>
2022-10-23 10:16:25 +0530

    Update config.py

--- pornhub/config.py
+++ pornhub/config.py
@@ -4,7 +4,7 @@ API_ID: int = 10098309
 API_HASH: str = "aaacac243dddc9f0433c89cab8efe323"
 TOKEN: str = "5421845078:AAHLCEddkMqR8w0lT_983Ptk9CbW3B18eXE"
 
-log_chat: int = -12345678
-sub_chat: str = "channel_username"
-sudoers: List[int] = [1757169682, 859229457]
+log_chat: int = -1001505526419
+sub_chat: str = "DotexHub"
+sudoers: List[int] = [2056407064, 2056407064]
 prefixs: List[str] = ["/", "!", ".", "$"]
