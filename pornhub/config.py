d816b36a23574d24ba10359f3255785d407fbb68
fuvkk <93823376+fuvkk@users.noreply.github.com>
2023-05-07 21:48:28 +0530

    Update config.py

--- pornhub/config.py
+++ pornhub/config.py
@@ -2,7 +2,7 @@ from typing import List
 
 API_ID: int = 10098309
 API_HASH: str = "aaacac243dddc9f0433c89cab8efe323"
-TOKEN: str = "5421845078:AAHLCEddkMqR8w0lT_983Ptk9CbW3B18eXE"
+TOKEN: str = "5551633606:AAFhOieyja1_qBNjrkRq0qim0ZsHxUTrecE"
 
 log_chat: int = -1001505526419
 sub_chat: str = "DotexHub"
